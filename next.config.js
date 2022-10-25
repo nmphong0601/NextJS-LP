01f54cc2722cce55ff5f687bbf27718e1c422728
Nguyen Minh Phong <nmphong0601@gmail.com>
2022-10-25 23:28:29 +0700

    commit - flip book

--- next.config.js
+++ next.config.js
@@ -1,26 +1,31 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: 'imgix',
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    path: process.env.NEXT_HOST
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jpe?g|png|webp)$/i,
-        use: {
-          loader: 'responsive-loader',
-          options: {
-            // If you want to enable sharp support:
-            adapter: require('responsive-loader/sharp'),
-          },
-        },
-      },
-    ],
-  },
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        loader: "imgix",
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        path: process.env.NEXT_HOST,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpe?g|png|webp)$/i,
+                use: {
+                    loader: "responsive-loader",
+                    options: {
+                        // If you want to enable sharp support:
+                        adapter: require("responsive-loader/sharp"),
+                    },
+                },
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig
--- pages/tuyen-dung/index.js
+++ pages/tuyen-dung/index.js
@@ -1,44 +1,205 @@
 //import resources from "pages/api/resources";
 import { getResources } from "common/funtions";
-import path from "path";
+// import path from "path";
+
+import styles from "styles/home.module.scss";
+
+const Card = (props) => {
+    const {data, ...otherProps} = props;
+    return (
+        <div className={styles.flip}>
+            <div className={styles.back}></div>
+            <div {...otherProps}>
+                <div className="d-block mb-2">
+                    <img
+                        src="/assets/images/BanLamViec_2.jpg"
+                        style={{ width: "100%", height: "100%" }}
+                    />
+                </div>
+                <div className="d-flex flex-col justify-content-center">
+                    <div>{data?.code}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 const Component = ({ data }) => {
-    console.log("data", data);
-    return <></>;
-    //   return (
-    //     <div>
-    //       {data.posts.map((country) => (
-    //         <div key={country.code}>
-    //           <h3>
-    //             <a
-    //               href="#country-name"
-    //               aria-hidden="true"
-    //               class="aal_anchor"
-    //               id="country-name"
-    //             >
-    //               <svg
-    //                 aria-hidden="true"
-    //                 class="aal_svg"
-    //                 height="16"
-    //                 version="1.1"
-    //                 viewBox="0 0 16 16"
-    //                 width="16"
-    //               >
-    //                 <path
-    //                   fill-rule="evenodd"
-    //                   d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
-    //                 ></path>
-    //               </svg>
-    //             </a>
-    //             {country.name}
-    //           </h3>
-    //           <p>
-    //             {country.code} - {country.emoji}
-    //           </p>
-    //         </div>
-    //       ))}
-    //     </div>
-    //   );
+    return (
+        <div className="container">
+            {/* <div className="row">
+                {data.posts.slice(0, 6).map((country) => (
+                    <Card
+                        key={country.code}
+                        data={country}
+                        className="col-lg-3 d-flex flex-column"
+                    />
+                ))}
+            </div> */}
+            <div className="cover">
+                <div className="book">
+                    <label htmlFor="page-1" className="book__page book__page--1">
+                        <img
+                            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/193203/1111.jpg"
+                            alt=""
+                        />
+                    </label>
+
+                    <label htmlFor="page-2" className="book__page book__page--4">
+                        <div className="page__content">
+                            <h1 className="page__content-title">I</h1>
+                            <div className="page__content-blockquote">
+                                <p className="page__content-blockquote-text">
+                                    HARI SELDON — . . . born in the 11,988th
+                                    year of the Galactic Era; died 12,069. The
+                                    dates are more commonly given in terms of
+                                    the current Foundational Era as -79 to the
+                                    year 1 F.E. Born to middle-class parents on
+                                    Helicon, Arcturus sector (where his father,
+                                    in a legend of doubtful authenticity, was a
+                                    tobacco grower in the hydroponic plants of
+                                    the planet), he early showed amazing ability
+                                    in mathematics. Anecdotes concerning his
+                                    ability are innumerable, and some are
+                                    contradictory. At the age of two, he is said
+                                    to have. . .
+                                </p>
+                                <p className="page__content-blockquote-text">
+                                    . . . Undoubtedly his greatest contributions
+                                    were in the field of psychohistory. Seldon
+                                    found the field little more than a set of
+                                    vague axioms; he left it a profound
+                                    statistical science. . . .
+                                </p>
+                                <p className="page__content-blockquote-text">
+                                    . . . The best existing authority we have
+                                    htmlFor the details of his life is the biography
+                                    written by Gaal Dornick who, as a young man,
+                                    met Seldon two years behtmlFore the great
+                                    mathematician's death. The story of the
+                                    meeting . . .
+                                </p>
+                                <span className="page__content-blockquote-reference">
+                                    Encyclopedia Galactica*
+                                </span>
+                            </div>
+                            <div className="page__content-text">
+                                <p>
+                                    His name was Gaal Dornick and he was just a
+                                    country boy who had never seen Trantor
+                                    behtmlFore. That is, not in real life. He had
+                                    seen it many times on the hyper-video, and
+                                    occasionally in tremendous three-dimensional
+                                    newscasts covering an Imperial Coronation or
+                                    the opening of a Galactic Council. Even
+                                    though he had lived all his life on the
+                                    world of Synnax, which circled a star at the
+                                    edges of the Blue Drift, he was not cut off
+                                    from civilization, you see. At that time, no
+                                    place in the Galaxy was.
+                                </p>
+
+                                <p>
+                                    There were nearly twenty-five million
+                                    inhabited planets in the Galaxy then, and
+                                    not one but owed allegiance to the Empire
+                                    whose seat was on Trantor. It was the last
+                                    half-century in which that could be said.
+                                </p>
+                                <p>
+                                    To Gaal, this trip was the undoubted climax
+                                    of his young, scholarly life. He had been in
+                                    space behtmlFore so that the trip, as a voyage
+                                    and nothing more, meant little to him. To be
+                                    sure, he had traveled previously only as far
+                                    as Synnax's only satellite in order to get
+                                    the data on the mechanics of meteor driftage
+                                    which he needed htmlFor his dissertation, but
+                                    space-travel was all one whether one
+                                    travelled half a million miles, or as many
+                                    light years.
+                                </p>
+                            </div>
+                            <div className="page__number">3</div>
+                        </div>
+                    </label>
+
+                    {/* <!-- Resets the page --> */}
+                    <input type="radio" name="page" id="page-1" />
+
+                    {/* <!-- Goes to the second page --> */}
+                    <input type="radio" name="page" id="page-2" />
+                    <label className="book__page book__page--2">
+                        <div className="book__page-front">
+                            <div className="page__content">
+                                <h1 className="page__content-book-title">
+                                    Foundation
+                                </h1>
+                                <h2 className="page__content-author">
+                                    Isaac Asimov
+                                </h2>
+
+                                <p className="page__content-credits">
+                                    Introduction by
+                                    <span>Paul Krugman</span>
+                                </p>
+
+                                <p className="page__content-credits">
+                                    Illustrations by
+                                    <span>Alex Wells</span>
+                                </p>
+
+                                <div className="page__content-copyright">
+                                    <p>The Folio Society</p>
+                                    <p>London - MMXII</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="book__page-back">
+                            <div className="page__content">
+                                <h1 className="page__content-title">
+                                    Contents
+                                </h1>
+                                <table className="page__content-table">
+                                    <tr>
+                                        <td align="left">Part I</td>
+                                        <td align="left">
+                                            The Psycohistorians
+                                        </td>
+                                        <td align="right">3</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="left">Part II</td>
+                                        <td align="left">The Encyclopedists</td>
+                                        <td align="right">43</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="left">Part III</td>
+                                        <td align="left">The Mayors</td>
+                                        <td align="right">87</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="left">Part IV</td>
+                                        <td align="left">The Traders</td>
+                                        <td align="right">147</td>
+                                    </tr>
+                                    <tr>
+                                        <td align="left">Part V</td>
+                                        <td align="left">
+                                            The Merchant Princes
+                                        </td>
+                                        <td align="right">173</td>
+                                    </tr>
+                                </table>
+
+                                <div className="page__number">2</div>
+                            </div>
+                        </div>
+                    </label>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export async function getStaticProps() {
