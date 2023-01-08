e123fb4cc463b0cd716e320d0de5ebdea9f8fbb6
Nguyen Minh Phong <nmphong0601@gmail.com>
2023-01-08 22:38:21 +0700

    commit - demo monitor 01

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
+import { Gluten } from "@next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "styles/globals.scss";
 
 import { useEffect } from "react";
 
+const gluten = Gluten({
+    weight: ["400", "500", "600", "800"],
+    subsets: ["latin"],
+});
+
 function MyApp({ Component, pageProps }) {
     useEffect(() => {
         require("bootstrap/dist/js/bootstrap.bundle.min.js");
     }, []);
-    return <Component {...pageProps} />;
+    return (
+        <main className={gluten.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
 
 export default MyApp;
---
+++ pages/demo-hpbd/index.js
@@ -0,0 +1,43 @@
+//import resources from "pages/api/resources";
+// import { getResources } from "common/funtions";
+import { Children, cloneElement, useEffect, useRef, useState } from "react";
+// import path from "path";
+
+import staticData from "./data.json";
+
+/* components */
+import Monitor01 from "components/templates/monitor-01";
+import Monitor02 from "components/templates/monitor-02";
+
+import styles from "./index.module.scss";
+import Image from "next/image";
+
+const Component = ({ data }) => {
+    // const [hover, setHover] = useState(false);
+
+    return (
+        <div className="position-relative vw-100 vh-100">
+            <Monitor01 infor={staticData?.infor} />
+            <Monitor02 />
+        </div>
+    );
+};
+
+export async function getStaticProps() {
+    //const res = await fetch(`${process.env.NEXT_HOST}/api/resources`, {method: 'POST'});
+    //const resData = await res.json();
+
+    let dataJson = await import("./data.json").default;
+
+    // if (!dataJson) {
+    //     dataJson = await getResources("data.json");
+    // }
+
+    return {
+        props: {
+            data: dataJson || {},
+        },
+    };
+}
+
+export default Component;
