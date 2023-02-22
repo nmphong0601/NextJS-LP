28ad8052742740aebdb0bd1a7ac7cfb1d529d788
Nguyen Minh Phong <nmphong0601@gmail.com>
2023-02-22 21:41:28 +0700

    commit

--- components/atoms/FacebookChat/index.js
+++ components/atoms/FacebookChat/index.js
@@ -11,6 +11,7 @@ const FacebookChat = () => {
                 var chatbox = document.getElementById('fb-customer-chat');
                 chatbox.setAttribute("page_id", "525721164174881");
                 chatbox.setAttribute("attribution", "biz_inbox");
+                chatbox.setAttribute("greeting_dialog_display", "Hello Hello");
 
                 window.fbAsyncInit = function() {
                     FB.init({
